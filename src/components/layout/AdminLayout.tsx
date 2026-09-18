ed3a35a91217560509a3fea36a915b9ee74be432
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-18 13:17:47 +0600

    Let admin content use available width

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -50,7 +50,7 @@ export const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
         />
 
         {/* Page Main Content Area */}
-        <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 flex flex-col min-w-0">
+        <main className="flex-1 w-full max-w-none p-4 sm:p-6 lg:p-8 flex flex-col min-w-0">
           <ErrorBoundary>
             {children || <Outlet />}
           </ErrorBoundary>
