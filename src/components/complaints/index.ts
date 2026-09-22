cfe220a8629b0cdf8eb264f6784c3b9bbbe90967
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-22 08:05:47 +0600

    remove internal-only complaint barrel exports

--- src/components/complaints/index.ts
+++ src/components/complaints/index.ts
@@ -3,15 +3,12 @@ export * from './ComplaintSearch';
 export * from './ComplaintFilters';
 export * from './ComplaintTable';
 export * from './MobileComplaintCardList';
-export * from './ComplaintEmptyState';
 export * from './ComplaintSummaryCard';
-export type { ComplaintInfoSectionProps } from './ComplaintInfoSection';
 export { ComplaintInfoSectionWithHarassmentContext as ComplaintInfoSection } from './ComplaintInfoSectionWithHarassmentContext';
 export * from './ComplaintPartiesCard';
 export * from './ComplaintMediaViewer';
 export * from './ComplaintLocationCard';
 export * from './ComplaintTimeline';
-export type { ComplaintActionAreaProps } from './ComplaintActionArea';
 export { ComplaintActionAreaWithPublication as ComplaintActionArea } from './ComplaintActionAreaWithPublication';
 export * from './ComplaintVersionHistory';
 
