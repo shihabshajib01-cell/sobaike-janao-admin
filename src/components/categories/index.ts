bb3d01b6e7f5f2af5676cafcd5b6ac3a44da93db
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-22 08:05:35 +0600

    remove unused category barrel reexports

--- src/components/categories/index.ts
+++ src/components/categories/index.ts
@@ -2,9 +2,5 @@ export * from './CategoryFilters';
 export * from './SegmentTabs';
 export * from './TaxonomyTree';
 export * from './CategoryDetailDrawer';
-export * from './CategoryEditModal';
 export * from './CategoryEmptyState';
-
 export * from './CategoryCreateModal';
-
-export * from './FormBuilderModal';
