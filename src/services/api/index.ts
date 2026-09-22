9ba1b44849525e0686400e15d8a006797d443913
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-22 08:06:25 +0600

    narrow API barrel to consumed services

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -7,33 +7,14 @@ export { default as responseApi } from './responseApi';
 export * from './categoryApi';
 export { default as categoryApi } from './categoryApi';
 
-export * from './mapApi';
-export { default as mapApi } from './mapApi';
-
 export * from './dashboardApi';
 export { default as dashboardApi } from './dashboardApi';
 
 export * from './locationActivityService';
 export { default as locationActivityService } from './locationActivityService';
 
-export * from './reverseGeocodingService';
-export { default as reverseGeocodingService } from './reverseGeocodingService';
-
 export * from './roleApi';
 export { default as roleApi } from './roleApi';
-
-export * from './adminUserApi';
-export { adminUserApi } from './adminUserApi';
-
-export * from './notificationApi';
-export { default as notificationApi } from './notificationApi';
-
-export * from './auditLogApi';
-export { auditLogApi } from './auditLogApi';
-
-
-export * from './reportingFormApi';
-export { default as reportingFormApi } from './reportingFormApi';
 
 export * from './newsIntakeApi';
 export { default as newsIntakeApi } from './newsIntakeApi';
