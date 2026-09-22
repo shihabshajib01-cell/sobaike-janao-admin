9e3ec463340dd4b73a93bc47ed0edd84713e16d0
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-22 08:05:39 +0600

    remove unused location barrel reexports

--- src/components/locationActivity/index.ts
+++ src/components/locationActivity/index.ts
@@ -1,7 +1,5 @@
-export * from './LocationPermissionBadge';
 export * from './LocationActivityStatsCards';
 export * from './LocationActivityFilters';
 export * from './LocationActivityTable';
 export * from './MobileLocationActivityCardList';
 export * from './LocationActivityDetailDrawer';
-export * from './LocationActivityEmptyState';
