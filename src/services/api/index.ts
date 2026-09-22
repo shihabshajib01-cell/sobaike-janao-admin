ccfc4ddbd3409ff9fef562b8b23534a7a43a3785
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-22 08:24:27 +0600

    narrow api/index.ts barrel exports

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,20 +1,8 @@
-export * from './complaintApi';
 export { default as complaintApi } from './complaintApi';
-
-export * from './responseApi';
 export { default as responseApi } from './responseApi';
-
-export * from './categoryApi';
 export { default as categoryApi } from './categoryApi';
-
-export * from './dashboardApi';
+export type { TaxonomyBundle } from './categoryApi';
 export { default as dashboardApi } from './dashboardApi';
-
-export * from './locationActivityService';
 export { default as locationActivityService } from './locationActivityService';
-
-export * from './roleApi';
 export { default as roleApi } from './roleApi';
-
-export * from './newsIntakeApi';
 export { default as newsIntakeApi } from './newsIntakeApi';
