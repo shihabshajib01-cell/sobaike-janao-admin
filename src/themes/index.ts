d28219f926a4c90e24d849ef2070162f70211176
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-22 07:59:04 +0600

    narrow theme barrel to used public API

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,6 +1,1 @@
-export { useTheme } from './ThemeProvider';
-export type { ThemeMode, ThemeProviderProps } from './ThemeProvider';
-export { ThemeProvider } from './ThemeProvider';
-export * from './tokens';
-export * from './light';
-export * from './dark';
+export { ThemeProvider, useTheme } from './ThemeProvider';
